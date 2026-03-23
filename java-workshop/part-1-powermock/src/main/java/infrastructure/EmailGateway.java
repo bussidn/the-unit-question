599a4286c8613c0352ac4340596afe960a03bbcc
bussid <dorian.bussi@sundayapp.com>
2026-03-23 15:53:02 +0100

    fix: remove accidentally committed exercise solution files

--- java-workshop/part-1-powermock/src/main/java/infrastructure/EmailGateway.java
+++
@@ -1,9 +0,0 @@
-package infrastructure;
-
-public class EmailGateway {
-    public static void send(String to, String subject, String body) {
-        // External email service - fails if not properly configured!
-        throw new RuntimeException("Email server not configured! Cannot send to: " + to);
-    }
-}
-
